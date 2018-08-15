24db7ecb3d22537af5fbbbb665d147143cab926d
datdta95 <datdta95@gmail.com>
2018-08-15 10:28:49 +0700

    phương thức tính giá trị tuyệt đối

---
+++ src/main/java/AbsoluteNumberCalculator.java
@@ -0,0 +1,12 @@
+public class AbsoluteNumberCalculator {
+    public static void main(String[] args) {
+//        int number = 0;
+//        System.out.println(findAbsolute(number));
+    }
+
+    public static int findAbsolute(int number) {
+        if (number < 0) {
+            return -number;
+        } else return number;
+    }
+}
